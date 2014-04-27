62797e3a72423c91966a63b0c6cfb4efb21863d8
thepeg <marco.pegoraro@gmail.com>
2014-04-27 11:16:35 +0200

    add info as default command

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 var commands = {
+    info: require('./cmd/info'),
     init: require('./cmd/init'),
     develop: require('./cmd/develop')
 };
 
 exports.run = function() {
     
-    var command = process.argv[2] || 'develop';
+    var command = process.argv[2] || 'info';
     
     if (commands[command]) {
         commands[command].start();
---
+++ cmd/info.js
@@ -0,0 +1,15 @@
+
+exports.start = function() {
+    console.log('');
+    console.log('');
+    console.log('------------ PoliteJS WorkspaceCLI v0.1.0 --------------');
+    console.log('wks init:    initialize an emtpy folder to host a new ');
+    console.log('             Workspace boilerplate project.');
+    console.log('');
+    console.log('wks develop: start a development session on the current');
+    console.log('             folder, serve debug build on 8080, listen');
+    console.log('             to source files changes and do rebuild.');
+    console.log('--------------------------------------------------------');
+    console.log('');
+    console.log('');
+};
