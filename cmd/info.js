8585bf54f02c86f616be0a2da1e2f22b93464641
thepeg <marco.pegoraro@gmail.com>
2014-04-30 08:36:37 +0200

    update wks info

--- cmd/info.js
+++ cmd/info.js
@@ -6,9 +6,14 @@ exports.start = function() {
     console.log('wks init:    initialize an emtpy folder to host a new ');
     console.log('             Workspace boilerplate project.');
     console.log('');
-    console.log('wks develop: start a development session on the current');
-    console.log('             folder, serve debug build on 8080, listen');
-    console.log('             to source files changes and do rebuild.');
+    console.log('wks develop: start a development session on current');
+    console.log('             project, generate a debug build, run tests');
+    console.log('             and serve everithing through a builtin server');
+    console.log('');
+    console.log('wks release: generate an optimised build for the project.');
+    console.log('             everything should be minimized and inlined.');
+    console.log('             all tests are executed and the build is');
+    console.log('             served through a builtin server.');
     console.log('--------------------------------------------------------');
     console.log('');
     console.log('');
